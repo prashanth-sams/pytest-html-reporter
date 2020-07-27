ed703c6544727e8cd5504305f9dadc5b714fa7d1
prashanth-sams <sams.prashanth@gmail.com>
2020-07-27 12:43:49 +0400

    fixes

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name='pytest-html-reporter',
-    version='0.0.1',
+    version='0.0.2',
     author='Prashanth Sams',
     author_email='sams.prashanth@gmail.com',
     maintainer='Prashanth Sams',
