c3646ef0d9212cf6948de1bdec9edab426d1f758
prashanth-sams <sams.prashanth@gmail.com>
2020-08-23 16:49:01 +0400

    #99 #102

--- pytest_html_reporter/plugin.py
+++ pytest_html_reporter/plugin.py
@@ -7,6 +7,7 @@ from os.path import isfile, join
 import json
 import glob
 from collections import Counter
+import codecs
 
 _total = _executed = 0
 _pass = _fail = 0
@@ -505,6 +506,8 @@ class HTMLReporter:
         template_text = template_text.replace("__tpass__", str(tpass))
         template_text = template_text.replace("__tfail__", str(tfail))
         template_text = template_text.replace("__tskip__", str(tskip))
+        template_text = template_text.replace("__css_styles__",
+                                              str(codecs.open('pytest_html_reporter/style.css', encoding='utf-8').read()))
         return template_text
 
     def generate_json_data(self, base):
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name='pytest-html-reporter',
-    version='0.1.4',
+    version='0.1.5',
     author='Prashanth Sams',
     author_email='sams.prashanth@gmail.com',
     maintainer='Prashanth Sams',
